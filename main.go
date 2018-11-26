0dae44acd1f28f3c1f573c0ce889adfc023be389
PUGE <100284685@qq.com>
2018-11-26 16:05:39 +0800

    Update and rename tool.go to main.go

--- main.go
+++ main.go
@@ -43,3 +43,31 @@ func Contain(obj interface{}, target interface{}) bool {
 	}
 	return false
 }
+// 检查端口是否被占用
+func CheckPortOccupy(port int) bool {
+	// 执行系统命令
+	out, err := exec.Command("netstat", "-ano").Output()
+	if err != nil {
+		log.Fatalf("exec command error: %s", err)
+	}
+	// 判断端口是否被占用
+	return strings.Contains(string(out), fmt.Sprintf(":%d", port))
+}
+
+// Contain 判断obj是否在target中，target支持的类型arrary,slice,map
+func Contain(obj interface{}, target interface{}) bool {
+	targetValue := reflect.ValueOf(target)
+	switch reflect.TypeOf(target).Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < targetValue.Len(); i++ {
+			if targetValue.Index(i).Interface() == obj {
+				return true
+			}
+		}
+	case reflect.Map:
+		if targetValue.MapIndex(reflect.ValueOf(obj)).IsValid() {
+			return true
+		}
+	}
+	return false
+}
